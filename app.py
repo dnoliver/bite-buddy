aa638ac5602108c9e3e28409cb8e5a7d1b23c05a
Nicolas Oliver <dario.n.oliver@intel.com>
2025-02-17 15:19:34 -0800

    Add more files to the prototype

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from haystack.components.converters import HTMLToDocument
 from haystack.components.builders import ChatPromptBuilder
 from haystack.components.generators.chat import OpenAIChatGenerator
 from haystack.dataclasses import ChatMessage
+from haystack.components.audio import RemoteWhisperTranscriber
 
 import gradio as gr
 
@@ -26,6 +27,7 @@ prompt_template = [
 
 prompt_builder = ChatPromptBuilder(template=prompt_template)
 llm = OpenAIChatGenerator()
+transcriber = RemoteWhisperTranscriber()
 
 pipeline = Pipeline()
 pipeline.add_component("fetcher", fetcher)
@@ -37,19 +39,42 @@ pipeline.connect("fetcher.streams", "converter.sources")
 pipeline.connect("converter.documents", "prompt.documents")
 pipeline.connect("prompt.prompt", "llm")
 
-def ask_question(query):
-    result = pipeline.run({
-        "fetcher": {"urls": ["https://haystack.deepset.ai/overview/quick-start"]},
-        "prompt": {"query": query}
-    })
-    return result["llm"]["replies"][0].text
+with gr.Blocks() as demo:
+    gr.Markdown("# Local Voice Assistant")
+    gr.Markdown(
+        """
+        This is a voice assistant that can answer questions based on the contents of a website. 
+        You can ask a question by speaking into the microphone. 
+        You can type the question in the text box as well.
+        Then click "Run" to get the answer.
+        """
+    )
+    audio = gr.Audio(label="Audio", sources=["microphone"], type="filepath")
+    url = gr.Textbox(label="URL", placeholder="https://haystack.deepset.ai/overview/quick-start")
+    query = gr.Textbox(label="Question")
+    run = gr.Button(value="Run")
+    result = gr.Textbox(label="Answer")
 
-iface = gr.Interface(
-    fn=ask_question,
-    inputs="text",
-    outputs="text",
-    title="Ask Questions to LLM",
-    description="Enter a question to get an answer from the LLM based on the content of the provided URL."
-)
-iface.launch()
+    def run_query(url, query):
+        response = pipeline.run({
+          "fetcher": {"urls": [url]},
+          "prompt": {"query": query}
+        })
+        return response["llm"]["replies"][0].text
+
+    def transcribe_audio(audio_file):
+      if audio_file is None:
+          return ""
+
+      transcription = transcriber.run(sources=[audio_file])
+      return transcription["documents"][0].content
+
+    audio.input(transcribe_audio, inputs=[audio], outputs=[query])
+    query.submit(run_query, inputs=[url, query], outputs=[result])
+    run.click(run_query, inputs=[url, query], outputs=[result])
+
+if __name__ == "__main__":
+    demo.launch(
+        allowed_paths=".tmp"
+    )
 
